58352c2ce71cbbe32d5efb3ae1318fd1c0fb96c8
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-08-06 06:16:21 +0700

    setting up react

---
+++ src/pages/ManagerHome/index.tsx
@@ -0,0 +1,8 @@
+
+const ManagerHome = () => {
+  return (
+    <div>Manager Home</div>
+  )
+}
+
+export default ManagerHome
---
+++ src/router/index.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import ManagerHome from "../pages/ManagerHome";
+import SignIn from "../pages/SignIn";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ManagerHome />,
+  },
+  {
+    path: "/manager/sign-in",
+    element: <SignIn />,
+  }
+]);
+
+export default router;
