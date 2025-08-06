451084901a06268e71cd148350fe25df2fa91111
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-08-07 06:49:43 +0700

    add page sign in and pricing to router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,25 @@
 import { createBrowserRouter } from 'react-router'
 import ManagerHome from '../pages/ManagerHome'
+import Pricing from '../pages/Pricing'
 import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
 
 const router = createBrowserRouter([
   {
-    path: '/manager/sign-in',
+    path: '/',
     element: <ManagerHome />,
   },
   {
-    path: '/',
+    path: '/manager/sign-in',
     element: <SignIn />,
+  },
+  {
+    path: '/manager/sign-up',
+    element: <SignUp />,
+  },
+  {
+    path: '/pricing',
+    element: <Pricing />,
   },
 ])
 
