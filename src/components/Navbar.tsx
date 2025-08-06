d6d005dafe986aca82c654cb5a008355627bf7c5
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-08-07 06:48:29 +0700

    add conditional additional links navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = ({ additionalLinks }: NavbarProps) => {
             <Link to="/">Home</Link>
           </li>
           <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-            <Link to="pricing.html">Pricing</Link>
+            <Link to="/pricing">Pricing</Link>
           </li>
           <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
             <Link to="#">Features</Link>
@@ -28,7 +28,24 @@ const Navbar = ({ additionalLinks }: NavbarProps) => {
           </li>
         </ul>
       </div>
-      <div className="flex items-center gap-3">{additionalLinks}</div>
+      <div className="flex items-center gap-3">
+        {additionalLinks ? (
+          additionalLinks
+        ) : (
+          <>
+            <Link to="#">
+              <div className="flex items-center gap-3 w-fit rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#070B24] border-[#24283E] shadow-[-10px_-6px_10px_0_#181A35_inset]">
+                <span className="font-semibold text-white">My Dashboard</span>
+              </div>
+            </Link>
+            <Link to="/manager/sign-up">
+              <div className="flex items-center gap-3 w-fit rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#662FFF] border-[#8661EE] shadow-[-10px_-6px_10px_0_#7F33FF_inset]">
+                <span className="font-semibold text-white">Sign Up</span>
+              </div>
+            </Link>
+          </>
+        )}
+      </div>
     </nav>
   )
 }
