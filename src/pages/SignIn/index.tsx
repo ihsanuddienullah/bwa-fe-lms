e5e6879fc9e8dd54dce52c58bc638c27843e943e
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-08-07 06:49:27 +0700

    update page sign in

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router'
+import Navbar from '../../components/Navbar'
 
 const SignIn = () => {
   return (
@@ -10,41 +11,22 @@ const SignIn = () => {
           alt=""
         />
       </div>
-      <nav className="flex items-center justify-between p-[30px]">
-        <div className="flex items-center gap-[60px]">
-          <img
-            src="/assets/images/logos/logo.svg"
-            className="flex shrink-0"
-            alt="logo"
-          />
-          <ul className="flex items-center gap-10">
-            <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-              <Link to="pricing.html">Pricing</Link>
-            </li>
-            <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-              <Link to="#">Features</Link>
-            </li>
-            <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-              <Link to="#">Testimonials</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="flex items-center gap-3">
-          <Link to="#">
-            <div className="flex items-center gap-3 w-fit rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#070B24] border-[#24283E] shadow-[-10px_-6px_10px_0_#181A35_inset]">
-              <span className="font-semibold text-white">My Dashboard</span>
-            </div>
-          </Link>
-          <Link to="/manager/sign-up">
-            <div className="flex items-center gap-3 w-fit rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#662FFF] border-[#8661EE] shadow-[-10px_-6px_10px_0_#7F33FF_inset]">
-              <span className="font-semibold text-white">Sign Up</span>
-            </div>
-          </Link>
-        </div>
-      </nav>
+      <Navbar
+        additionalLinks={
+          <>
+            <Link to="#">
+              <div className="flex items-center gap-3 w-fit rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#070B24] border-[#24283E] shadow-[-10px_-6px_10px_0_#181A35_inset]">
+                <span className="font-semibold text-white">My Dashboard</span>
+              </div>
+            </Link>
+            <Link to="/manager/sign-up">
+              <div className="flex items-center gap-3 w-fit rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#662FFF] border-[#8661EE] shadow-[-10px_-6px_10px_0_#7F33FF_inset]">
+                <span className="font-semibold text-white">Sign Up</span>
+              </div>
+            </Link>
+          </>
+        }
+      />
       <form className="flex flex-col w-[400px] h-fit rounded-[20px] border border-[#262A56] p-[30px] gap-[30px] bg-[#080A2A] m-auto">
         <div>
           <h1 className="font-bold text-[26px] leading-[39px] text-white">
