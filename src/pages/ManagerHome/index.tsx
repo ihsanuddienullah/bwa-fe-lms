8c60742e92630fd81e95694711d439e1c73198b5
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-08-09 15:03:43 +0700

    update manager home with layout

--- src/pages/ManagerHome/index.tsx
+++ src/pages/ManagerHome/index.tsx
@@ -1,7 +1,39 @@
+import { Link } from 'react-router'
+import LatestCourse from './_components/LatestCourses'
+import LatestStudents from './_components/LatestStudents'
+import StatsOverview from './_components/StatsOverview'
 
 const ManagerHome = () => {
   return (
-    <div>Manager Home</div>
+    <div>
+      <header className="flex items-center justify-between gap-[30px]">
+        <div>
+          <h1 className="font-extrabold text-[28px] leading-[42px]">
+            Overview
+          </h1>
+          <p className="text-[#838C9D] mt-[1]">Grow your company quickly</p>
+        </div>
+        <div className="flex items-center gap-3">
+          <Link
+            to="#"
+            className="w-fit rounded-full border border-[#060A23] p-[14px_20px] font-semibold text-nowrap"
+          >
+            Customize
+          </Link>
+          <Link
+            to=""
+            className="w-fit rounded-full p-[14px_20px] font-semibold text-[#FFFFFF] bg-[#662FFF] text-nowrap"
+          >
+            Export Data
+          </Link>
+        </div>
+      </header>
+      <StatsOverview />
+      <div className="grid grid-cols-2 gap-[30px]">
+        <LatestCourse />
+        <LatestStudents />
+      </div>
+    </div>
   )
 }
 
